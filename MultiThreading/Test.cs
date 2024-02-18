0cb794e3e4ec9d27fdc4b73a13363bd7391d9218
KovalYevhenii <kovalyevhenii@gmail.com>
2024-02-18 01:13:45 +0100

    Add project files.

---
+++ MultiThreading/Test.cs
@@ -0,0 +1,31 @@
+﻿using System.Diagnostics;
+
+namespace MultiThreading;
+internal class Test
+{
+    public static long SimpleSum(int[] data)
+    {
+        return data.Sum(x => (long)x);
+
+
+    }
+    public static void SimpleSumParallel(int[] data)
+    {
+        Thread thread = new Thread(() =>
+        {
+            var res = data.Sum(x => (long)x);
+        });
+        thread.Start();
+    }
+    public void SimpleSumPlinq(int[] ints)
+    {
+
+    }
+    public static void MeasureExecutionTime(string method, Action action)
+    {
+        Stopwatch stopwatch = Stopwatch.StartNew();
+        action?.Invoke();
+        stopwatch.Stop();
+        Console.WriteLine($"{method}| {stopwatch.ElapsedMilliseconds}ms");
+    }
+}
